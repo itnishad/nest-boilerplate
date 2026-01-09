d733d839ecf45b4dd297a2b832de3e96fa7f6ec2
Nishad <mdfaysalhassan@gmail.com>
2026-01-09 22:54:11 +0600

    add path prefix 'api' and versioning 'v1'

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get<ConfigService>(ConfigService);
+
+  app.setGlobalPrefix('api');
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+
   await app.listen(configService.get<number>('port') ?? 3000);
 }
 bootstrap();
