1a3311ffaac22896f04a7e2ec23ebe66244a8d39
Nishad <mdfaysalhassan@gmail.com>
2026-01-21 21:44:14 +0600

    add: @nestjs/swagger for API documentation and setup Swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 import { VersioningType } from '@nestjs/common';
@@ -13,6 +14,19 @@ async function bootstrap() {
   app.enableVersioning({
     type: VersioningType.URI,
     defaultVersion: '1',
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Nest example')
+    .setDescription('The Nest API description')
+    .setVersion('1.0')
+    .addTag('nest')
+    .addBearerAuth()
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, documentFactory, {
+    jsonDocumentUrl: 'swagger/json'
   });
 
   await app.listen(configService.get<number>('port') ?? 3000);
