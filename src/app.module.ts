fc6abe5838701982eb2dc1d50d0a3a10813d6751
Nishad <mdfaysalhassan@gmail.com>
2026-02-23 22:27:10 +0600

    feat(redis): add Redis module and service for Redis integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
+import { RedisModule } from './common/redis/redis.module';
 import config from '../config'
 
 
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal: true, load: [config]}), DatabaseModule],
+  imports: [ConfigModule.forRoot({isGlobal: true, load: [config]}), DatabaseModule, RedisModule],
   controllers: [AppController],
   providers: [AppService],
 })
